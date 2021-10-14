5b3528ff3739f975eb16d5ef1c5d31cf9cba06c9
Lisa Wimmer <68507897+liwim100@users.noreply.github.com>
2021-10-14 08:46:34 +0200

    preliminary final version
    Changes in sweep, transfer_operators

--- int_nodes_test.py
+++ int_nodes_test.py
@@ -1,45 +1,55 @@
 import numpy as np
+import numpy.polynomial.legendre as legendre
 
-# Input:
-# a, b - Interval of the quadrature nodes which are determined
-# x    - nodes which are transformed
 
 def int_nodes(a, b, n):
+    
+    # Input:
+    # a, b - Interval of the quadrature nodes which are determined
+    # n    - number of nodes
+    
     xtilde = np.zeros(n)
 
-    if n == 3:
+    #if n == 3:
         # Gauss-Lobatto nodes for n=3
-        x = np.zeros(n)
+    #    x = np.zeros(n)
         
-        x[0] = -1
-        x[1] = 0
-        x[2] = 1
-        for i in range(0, np.shape(x)[0]):
-            xtilde[i] = (b - a) / 2 * x[i] + (a + b) / 2
+    #    x[0] = -1
+    #    x[1] = 0
+    #    x[2] = 1
+    #    for i in range(0, np.shape(x)[0]):
+    #        xtilde[i] = (b - a) / 2 * x[i] + (a + b) / 2
             
-    elif n == 4:
-        x = np.zeros(n)
+    #elif n == 4:
+    #    x = np.zeros(n)
         
-        x[0] = -1
-        x[1] = -np.sqrt(1/5)
-        x[2] = -x[1]
-        x[3] = -x[0]
-        for i in range(0, np.shape(x)[0]):
-            xtilde[i] = (b - a) / 2 * x[i] + (a + b) / 2
+    #    x[0] = -1
+    #    x[1] = -np.sqrt(1/5)
+    #    x[2] = -x[1]
+    #    x[3] = -x[0]
+    #    for i in range(0, np.shape(x)[0]):
+    #        xtilde[i] = (b - a) / 2 * x[i] + (a + b) / 2
             
-    elif n == 5:
+    #elif n == 5:
         # Gauss-Lobatto nodes for n=5
-        x = np.zeros(n)
+    #    x = np.zeros(n)
         
-        x[0] = -1
-        x[1] = -np.sqrt(3/7)
-        x[2] = 0
-        x[3] = np.sqrt(3/7)
-        x[4] = 1
-        for i in range(0, np.shape(x)[0]):
-            xtilde[i] = (b - a) / 2 * x[i] + (a + b) / 2
+    #    x[0] = -1
+    #    x[1] = -np.sqrt(3/7)
+    #    x[2] = 0
+    #    x[3] = np.sqrt(3/7)
+    #    x[4] = 1
+    #    for i in range(0, np.shape(x)[0]):
+    #        xtilde[i] = (b - a) / 2 * x[i] + (a + b) / 2
             
-    else:
-        print("No nodes implemented.")
+    #else:
+    #    print("No nodes implemented.")
 
+    
+    roots = legendre.legroots(legendre.legder(np.array([0] * (n - 1) + [1], dtype=np.float64)))
+    xtilde = np.array(np.append([-1.0], np.append(roots, [1.0])), dtype=np.float64)
+
+    xtilde = (a * (1 - xtilde) + b * (1 + xtilde)) / 2
+    
+    
     return xtilde
--- transfer_operators_test.py
+++ transfer_operators_test.py
@@ -1,13 +1,14 @@
 import numpy as np
-from numpy.fft import fft, ifft
+from numpy.fft import fft, ifft, rfft, irfft
 import scipy
 from scipy import signal
+import warnings
+warnings.filterwarnings('ignore')
 
-def interpolation(u, u_M, dtf, Mc, nxc, Mf, nxf, tf):
+
+def interpolation(u, Mc, nxc, uf, Mf, nxf, tc, tf):
     # Input:
     # u        - the vector u which has the size nx + 1
-    # u_M      - initial value at 0-th node
-    # dtf      - time steps on fine level
     # Mc       - number of coarse intermediate nodes plus 1
     # nxc      - number of coarse spatial nodes
     # Mf       - number of fine intermediate nodes plus 1
@@ -22,85 +23,99 @@ def interpolation(u, u_M, dtf, Mc, nxc, Mf, nxf, tf):
         f = lambda x: u0 * ((x - tau2)/(tau0 - tau2)) * ((x - tau4)/(tau0 - tau4)) + u2 * ((x - tau0)/(tau2 - tau0)) * ((x - tau4)/(tau2 - tau4)) + u4 * ((x - tau0)/(tau4 - tau0)) * ((x - tau2)/(tau4 - tau2))
         return f(z)
         
-    uInt = np.zeros(nxf * Mf, dtype='cfloat')
+    uInt = np.zeros(nxf * Mf, dtype='float')
+    tmp_u = np.zeros((nxf//2)+1, dtype='cfloat')
         
     # First, a spatial interpolation is used with FFT
-    uf0hat = np.zeros(nxf, dtype='cfloat')
-    uf2hat = np.zeros(nxf, dtype='cfloat')
-    uf4hat = np.zeros(nxf, dtype='cfloat')
+    tmp_uf0 = np.zeros((nxf//2)+1, dtype=np.complex128)
+    tmp_uf2 = np.zeros((nxf//2)+1, dtype=np.complex128)
+    tmp_uf4 = np.zeros((nxf//2)+1, dtype=np.complex128)
+    tmp_uf = np.zeros(3*nxf, dtype='float')
+    uf0 = np.zeros(nxf, dtype='float')
+    uf1 = np.zeros(nxf, dtype='float')
+    uf2 = np.zeros(nxf, dtype='float')
+    uf3 = np.zeros(nxf, dtype='float')
+    uf4 = np.zeros(nxf, dtype='float')
+    
     
     if np.shape(u)[0] == nxc:
-        uInt = np.zeros(nxf, dtype='cfloat')
+
+        uInt = np.zeros(nxf, dtype='float')
         
-        # Only a spatial interpolation is done
-        uInt[0:nxc//2] = 2*u[0:nxc//2]
-        uInt[3*nxc//2:nxf] = 2*u[nxc//2:nxc]
+        tmp = rfft(u)
+        
+        tmp_u[0:nxc//2] = tmp[0:nxc//2]
+        tmp_u[-1] = tmp[-1]
+        
+        uInt = irfft(tmp_u * 2)
         
     else:
-        uf0hat[0:nxc//2] = 2*u[0:nxc//2]
-        uf2hat[0:nxc//2] = 2*u[nxc:nxc+nxc//2]
-        uf4hat[0:nxc//2] = 2*u[2*nxc:2*nxc+nxc//2]
         
-        uf0hat[3*nxc//2:nxf] = 2*u[nxc//2:nxc]
-        uf2hat[3*nxc//2:nxf] = 2*u[nxc+nxc//2:2*nxc]
-        uf4hat[3*nxc//2:nxf] = 2*u[2*nxc+nxc//2:3*nxc]
-
-        uf1hat = np.zeros(nxf, dtype='cfloat')
-        uf3hat = np.zeros(nxf, dtype='cfloat')
+        tmp_uc0 = rfft(u[0:nxc])
+        tmp_uc2 = rfft(u[nxc:2*nxc])
+        tmp_uc4 = rfft(u[2*nxc:3*nxc])
+        
+        tmp_uf0[0:nxc//2] = tmp_uc0[0:nxc//2]
+        tmp_uf2[0:nxc//2] = tmp_uc2[0:nxc//2]
+        tmp_uf4[0:nxc//2] = tmp_uc4[0:nxc//2]
+        
+        tmp_uf0[-1] = tmp_uc0[-1]
+        tmp_uf2[-1] = tmp_uc2[-1]
+        tmp_uf4[-1] = tmp_uc4[-1]
+        
+        uf0 = 2 * irfft(tmp_uf0)
+        uf2 = 2 * irfft(tmp_uf2)
+        uf4 = 2 * irfft(tmp_uf4)
+        
                        
         for l in range(0, nxf):
-            uf1hat[l] = polynomial_interpolation(tf[0], uf0hat[l], tf[2], uf2hat[l], tf[4], uf4hat[l], tf[1])
-            uf3hat[l] = polynomial_interpolation(tf[0], uf0hat[l], tf[2], uf2hat[l], tf[4], uf4hat[l], tf[3])
+            uf1[l] = polynomial_interpolation(tf[0], uf0[l], tf[2], uf2[l], tf[4], uf4[l], tf[1])
+            uf3[l] = polynomial_interpolation(tf[0], uf0[l], tf[2], uf2[l], tf[4], uf4[l], tf[3])
         
         # The interpolated u's to the different time nodes will be collected
-        uf0uf1 = np.concatenate((uf0hat, uf1hat), axis=0)
-        uf0uf1uf2 = np.concatenate((uf0uf1, uf2hat), axis=0)
-        uf0uf1uf2uf3 = np.concatenate((uf0uf1uf2, uf3hat), axis=0)
-        uInt = np.concatenate((uf0uf1uf2uf3, uf4hat), axis=0)         
+        uf0uf1 = np.concatenate((uf0, uf1), axis=0)
+        uf0uf1uf2 = np.concatenate((uf0uf1, uf2), axis=0)
+        uf0uf1uf2uf3 = np.concatenate((uf0uf1uf2, uf3), axis=0)
+        uInt = np.concatenate((uf0uf1uf2uf3, uf4), axis=0)         
     
     return uInt
 
-def restriction(u, Mc, nxc, Mf, nxf):
+def restriction(u, Mc, nxc, Mf, nxf, tc, tf):
     # Input:
-    # u        - the vector u which has the size nx + 1
-    # Mc       - number of coarse intermediate nodes plus 1
+    # u        - the vector u which has the size nxf or Mf*nxf
+    # Mc       - number of coarse collocation nodes
     # nxc      - number of coarse spatial nodes
-    # Mf       - number of fine intermediate nodes plus 1
+    # Mf       - number of fine collocation nodes
     # nxf      - number of fine spatial nodes
     #
     # Output:
     # returns the restriction of vector u on a coarse level with injection
                 
-    uRestr = np.zeros(nxc * Mc, dtype='cfloat')
-
-    R = np.zeros((nxc, nxf))
+    uRestr = np.zeros(nxc * Mc, dtype='float')
+    tmp_uf = np.zeros(nxc * Mf, dtype='float')
+    tmp_uc = np.zeros(nxf * Mc, dtype='float')
     
-    # matrix choose only low frequencies because the vector has components in spectral space (pseudospectral discretization)
-    R[0:nxc//2, 0:nxc//2] = np.eye(nxc//2)
-    R[(nxc - nxc//2):nxc, (nxf - nxc//2):nxf] = np.eye(nxc//2)
-    R = 1/2 * R
-    ratio = nxf//nxc
+    # coarsening factor to restrict in space
+    coarsening_factor = nxf//nxc
     
     
     if Mc == 1 and Mf == 1:
-        tmp_u = np.fft.ifft(u)
-        uRestr = np.fft.fft(tmp_u[::ratio])
-        #uRestr = R.dot(u)
+        uRestr = u[::coarsening_factor]
     
     else:
+        # Restriction in space
         for l in range(0, Mf):
             if l == 0:
-                tmp_u = np.fft.ifft(u[l*nxf:l*nxf+nxf])
-                uRestr[l*nxc:l*nxc+nxc] = np.fft.fft(tmp_u[::ratio])
-                #uRestr[l*nxc:l*nxc+nxc] = R.dot(u[l*nxf:l*nxf+nxf])
+                tmp_u = u[l*nxf:l*nxf+nxf]
+                uRestr[l*nxc:l*nxc+nxc] = tmp_u[::coarsening_factor]
                 
             elif l%2 == 0 and l>0:
-                tmp2_u = np.fft.ifft(u[l*nxf:l*nxf+nxf])
-                uRestr[(l//2)*nxc:(l//2)*nxc+nxc] = np.fft.fft(tmp2_u[::ratio])
-                #uRestr[(l//2)*nxc:(l//2)*nxc+nxc] = R.dot(u[l*nxf:l*nxf+nxf])
+                tmp_u = u[l*nxf:l*nxf+nxf]
+                uRestr[(l//2)*nxc:(l//2)*nxc+nxc] = tmp_u[::coarsening_factor]
                 
             else:
                 continue 
+        
                 
 
     return uRestr
